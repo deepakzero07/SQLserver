98c347128df53acef56345cda3f5ddf662bdc5a8
Deepak S <105696859+deepakzero07@users.noreply.github.com>
2023-07-17 17:57:49 +0530

    Update notes.sql

--- July17/notes.sql
+++ July17/notes.sql
@@ -5,7 +5,7 @@ use vechicle
 create table car(wheels int,model varchar(20),mileage int, powertype varchar(20))
 
 insert into car values(4,'hondacity',10,'diesel')
-insert into car values(4,'audi R8',7,'diesel')
+insert into car values(4,'  audi R8',7,'diesel')
 insert into car values(4,'lambo',5,'lhy')
 insert into car values(5,'scorpio',6,'diesel')
 insert into car values(4,'civic',8,'diesel')
@@ -18,6 +18,8 @@ select * from car where mileage not between 5 and 8     /*using not between*/
 select * from car where mileage between 5 and 8  /*using between*/
 
 update car set powertype = 'electric' where model = 'audi R8'   /*update car row value*/
+
+select * from car
 
 select * from car order by model desc
 
@@ -51,6 +53,55 @@ select max(mileage) as varchar from car  /*maximum value*/
 
 select min(mileage) as varchar from car  /*minimum value*/
 
-select sum(mileage) as varchar from car  /*give range using and*/
+select sum(mileage) as varchar from car  /*use function sum*/
 
-select avg(mileage) as varchar from car  /*give range using and*/
+select avg(mileage) as varchar from car  /*use function avg*/
+
+/*string functions*/
+
+select upper(model) as upperCase from car/*convert to uppercase*/
+
+select lower(model) as lowerCase from car/*convert to lowercase*/
+
+select ltrim(model) 'left side space' from car/*trim from left side*/
+
+select rtrim(model) 'right side space' from car/*trim from right side*/
+
+select left(model,4) from car/*d----*/
+
+select right(model,4) from car
+
+select substring(model,2,4) as 'substring' from car
+
+select replicate(model,2) as 'replicate it twice' from car
+
+select model +' '+ powertype as address from car
+
+select CONCAT(model,' ',  powertype) as address from car
+
+select CURRENT_TIMESTAMP as gemmedate  /*gives date and time*/
+
+select getdate() as gemedate  /*gives date and time*/
+
+select convert (varchar(25), getdate()) todaydate  /*gives date and time by month*/
+
+select datename(day,getdate()) as day /*gives only date*/
+
+select datepart(month,getdate()) as month /*gives only month*/
+
+select datepart(year,getdate()) as year /*gives only year*/
+
+select dateadd(month,5,getdate()) as 'exactly today after 5 mnonths'
+
+select DATEDIFF(year, getdate(),'2023/07/17' ) as 'date difference'   /*gives year difference*/
+
+
+create table dataexample(id int,joining date,dob date)
+
+select * from dataexample
+
+insert into dataexample values(1,cast(getdate() as date)  , '2001/09/08') /*give input*/
+
+select datediff(year,dob,joining) from dataexample 
+
+
